bbe3e6e9af426e3c9fb52cd2dfaf00a2fd0eb17f
Dat Quach <datquach@kms-technology.com>
2019-03-27 17:42:50 +0700

    

---
+++ Katalon.Studio.app/Katalon Studio.app/Contents/Eclipse/configuration/resources/tools/carthage/0.18.1/.brew/carthage.rb
@@ -0,0 +1,23 @@
+class Carthage < Formula
+  desc "Decentralized dependency manager for Cocoa"
+  homepage "https://github.com/Carthage/Carthage"
+  url "https://github.com/Carthage/Carthage.git",
+      :tag => "0.18.1",
+      :revision => "84747d78c04e42e175059d5fa35468e50663c503",
+      :shallow => false
+  head "https://github.com/Carthage/Carthage.git", :shallow => false
+
+  depends_on :xcode => ["7.3", :build]
+
+  def install
+    system "make", "prefix_install", "PREFIX=#{prefix}"
+    bash_completion.install "Source/Scripts/carthage-bash-completion" => "carthage"
+    zsh_completion.install "Source/Scripts/carthage-zsh-completion" => "_carthage"
+    fish_completion.install "Source/Scripts/carthage-fish-completion" => "carthage.fish"
+  end
+
+  test do
+    (testpath/"Cartfile").write 'github "jspahrsummers/xcconfigs"'
+    system bin/"carthage", "update"
+  end
+end
